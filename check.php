72b4d7a93961fa7d303f047aaf53b6a0528a8e67
Manikant Goutam <manikantgautam3@gmail.com>
2026-07-14 16:18:56 +0530

    fix: upgrade jQuery to 3.7.1 to resolve CVE-2019-11358

--- check.php
+++ check.php
@@ -15,7 +15,7 @@ session_start();
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
 <!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
 <!-- Popper JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
